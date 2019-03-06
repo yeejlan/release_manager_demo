3f536695e1d333cdfc1b12854a7c9d42f90c2a2a
louis yu <yf_hello@163.com>
2019-03-06 17:47:01 +0800

    change LogModel.add

--- release_manager/src/main/kotlin/dao/ActionLogDao.kt
+++ release_manager/src/main/kotlin/dao/ActionLogDao.kt
@@ -4,20 +4,21 @@ import tiny.*
 import tiny.lib.*
 import release_manager.domain.*
 import java.util.Date
+import release_manager.library.Utils
 
 private val db = TinyRegistry["db.release_manager"] as TinyJdbc
 
 class ActionLogDao {
 
-	fun add(log: ActionLog): TinyResult<Long> {
+	fun add(userid: Long, username: String, action_name: String, return_message: String): TinyResult<Long> {
 
 		val p = mapOf(
-				"userid" to log.userid,
-				"username" to log.username,
-				"action_name" to log.action_name,
-				"return_message" to log.return_message,
-				"log_date" to log.log_date,
-				"log_ip" to log.log_ip
+				"userid" to userid,
+				"username" to username,
+				"action_name" to action_name,
+				"return_message" to return_message,
+				"log_date" to Date(),
+				"log_ip" to Utils.getClientIp()
 			)
 
 		val result = db.insert("""insert into action_log 
--- release_manager/src/main/kotlin/model/LogModel.kt
+++ release_manager/src/main/kotlin/model/LogModel.kt
@@ -3,7 +3,6 @@ package release_manager.model
 import tiny.*
 import release_manager.dao.*
 import release_manager.domain.*
-import release_manager.library.Utils
 import java.util.Date
 
 private val actionLogDao = ActionLogDao()
@@ -11,15 +10,7 @@ private val actionLogDao = ActionLogDao()
 class LogModel {
 	
 	fun add(userid: Long, username: String, action_name: String, return_message: String): TinyResult<Long> {
-		val actionLog = ActionLog(hashMapOf(
-				"userid" to userid,
-				"username" to username,
-				"action_name" to action_name,
-				"return_message" to return_message,
-				"log_date" to Date(),
-				"log_ip" to Utils.getClientIp()
-			))
-		return actionLogDao.add(actionLog)
+		return actionLogDao.add(userid, username, action_name, return_message)
 	}
 
 	fun list(dateFilter: Date?, NameFilter: String?, offset: Long = 0, pageSize: Int = 10): TinyResult<List<ActionLog>> {
